994643a1bdc13a224e76fbb5775469eeca6d3719
Shiva Aleti <aletishiva218@gmail.com>
2024-01-14 17:59:03 +0530

    updated multer code

--- Utils/uploadAvatar.js
+++ Utils/uploadAvatar.js
@@ -9,7 +9,6 @@ const uploadAvatar = multer({
         if(file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg")
         cb(null, true);
         else {
-            cb(null,false);
             return cb(new Error("Only .png, .jpg, .jpeg format are allowed"))
         }
       }
